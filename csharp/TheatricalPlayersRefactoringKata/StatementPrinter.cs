abe490e8ffadfc790701ad89c7196cbab447ef5e
Søren Trudsø Mahon <strudso@gmail.com>
2025-04-27 09:00:15 +0200

    r

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -15,20 +15,25 @@ namespace TheatricalPlayersRefactoringKata
 
             foreach(var perf in invoice.Performances) 
             {
-                var play = plays[perf.PlayID];
-                var thisAmount = AmounFor(perf, play);
+                var thisAmount = AmounFor(perf, PlayFor(plays, perf));
                 // add volume credits
                 volumeCredits += Math.Max(perf.Audience - 30, 0);
                 // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+                if ("comedy" == PlayFor(plays, perf).Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
                 // print line for this order
-                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", PlayFor(plays, perf).Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
             }
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
             return result;
+        }
+
+        private static Play PlayFor(Dictionary<string, Play> plays, Performance perf)
+        {
+            var play = plays[perf.PlayID];
+            return play;
         }
 
         private static int AmounFor(Performance perf, Play play)
